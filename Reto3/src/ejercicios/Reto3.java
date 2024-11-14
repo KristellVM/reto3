04c30f802fff0386b33f768e3fbbf81f86e3e832
KristellVM <krisvm31@gmail.com>
2024-11-14 09:43:35 +0100

    funciones 1

--- Reto3/src/ejercicios/Reto3.java
+++ Reto3/src/ejercicios/Reto3.java
@@ -9,7 +9,19 @@ public class Reto3 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		LocalDate fecha1 = funciones.dimeFecha("Dime la primera fecha (dd/MM/yyyy)", sc);
+		LocalDate fecha2;
+		do {
+			fecha2 = funciones.dimeFecha("Dime la segunda fecha que sea mayor que la primera (dd/MM/yyyy)", sc);
+		} while (fecha2.isBefore(fecha1));
+		
+		if(Funciones1.mismoAnio(fecha1, fecha2)) {
+			Funciones1.nombres(fecha1, fecha2, sc);
+		} else {
+			
+		}
 
 		
 	}
