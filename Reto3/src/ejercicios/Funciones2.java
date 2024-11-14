31a2fd9b50211c427f01e390fedb1cd31d99ab72
Alaa <abouzerraralaa05@gmail.com>
2024-11-14 09:32:03 +0100

    commit1

--- Reto3/src/ejercicios/Funciones2.java
+++ Reto3/src/ejercicios/Funciones2.java
@@ -1,5 +1,40 @@
 package ejercicios;
 
-public class Funciones2 {
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
+
+
+public class Funciones2 {
+public static void dias(LocalDate fecha1, LocalDate fecha2) {
+	int diasmes=0;
+	int media=0;
+	LocalDate mes;
+	int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+     for(int i =0;i<meses+1;i++) {
+    	 mes=fecha1.plusMonths(i);
+    	 int dias = mes.lengthOfMonth(); 
+    	 diasmes= diasmes+ dias;
+     }
+     media=diasmes/meses;
+     System.out.println("los dias = "+ diasmes +" la media = "+ media);
+     Month mes1=fecha1.getMonth(); 
+     switch (key) {
+	case value:
+		
+		break;
+
+	default:
+		break;
+	}
+     
+     
+    
+
+	
+	
+
+
+}
 }
--- Reto3/src/ejercicios/Reto3.java
+++ Reto3/src/ejercicios/Reto3.java
@@ -1,9 +1,17 @@
 package ejercicios;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import util.funciones;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		LocalDate fecha1=funciones.dimeFecha("introduce fecha 1",sc );
+		LocalDate fecha2=funciones.dimeFecha("introduce fecha 2",sc );
+		
 
 	}
 
