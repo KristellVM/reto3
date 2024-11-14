beaf396d47041bedf051a8028e7ab696890a998f
KristellVM <krisvm31@gmail.com>
2024-11-14 09:35:37 +0100

    funciones 1

--- Reto3/src/ejercicios/Reto3.java
+++ Reto3/src/ejercicios/Reto3.java
@@ -1,10 +1,16 @@
 package ejercicios;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import util.funciones;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }
